6436e053b44eb9bc525b1f6384fb8939272011a1
Aydin0509 <6004081@mborijnland.nl>
2022-03-21 10:43:12 +0100

    header partials

--- ALA.php
+++ ALA.php
@@ -1,19 +1,8 @@
 <?php
-$conn = new PDO('mysql:host=localhost;dbname=hobo2022', "root", "root");
+
+  require_once 'partials/header.php';
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="STYLESHEET" href="CSS/Style.css" type="text/css">
-    <title>Document</title>
-    
-</head>
-<body>
-
  <!-- Slideshow container -->
  <div class="slideshow-container">
 
