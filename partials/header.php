80c6a1217b31f09121e98c7965f27642113f5e14
sidneykempers <6008573@mborijnland.nl>
2022-04-04 10:40:42 +0200

    alles ff gecommit

--- partials/header.php
+++ partials/header.php
@@ -6,11 +6,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="STYLESHEET" href="CSS/Style.css" type="text/css">
-  <script src="https://kit.fontawesome.com/98b76129a9.js" crossorigin="anonymous"></script>
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="css/menu.css">
+    <link rel="stylesheet" href="CSS/Style.css" type="text/css">
+    <script src="https://kit.fontawesome.com/98b76129a9.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <script>
     </script>
