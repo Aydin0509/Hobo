9ab55e5f9f48e188390a4a2ea88c2830594eac7d
Jordy van der Ham <6013064@mborijnland.nl>
2022-04-15 11:39:27 +0200

    videoplayer toegevoegd

---
+++ Seriescherm.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<?php
+
+  require_once 'partials/header.php';
+  require_once 'backend/class/Search.php';
+  require_once 'backend/class/Serie.php';
+
+
+  ?> 
+<body>
+    
+
+<section class="videoplayer">
+<video controls src="images/Ozark.mp4" width="1000px";></video>
+</video>
+
+</section>
+<style>
+    .videoplayer {position: absolute; margin-left: 240px; margin-top: 60px;}
+
+</style>
+
+
+
+
+
+
+
+
+</body>
+</html>
